b2d7750e9eb0c1dd96590d78f06738ae719f967e
Just-vikas91 <mr.hacketh91@gmail.com>
2024-03-19 20:28:45 +0530

    Started on backend along with solving frontend id errors

--- app/src/main/java/com/example/langtrans/MainActivity.java
+++ app/src/main/java/com/example/langtrans/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.langtrans;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner fromSpinner, toSpinner;
+    private TextInputEditText sourceEdt;
+    private ImageView micTV;
+    private MaterialButton translateBtn;
+    private TextView translatedTV;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fromSpinner = findViewById(R.id.idFromSpinner);
+        toSpinner = findViewById(R.id.idToSpinner);
+        sourceEdt = findViewById(R.id.idEdtSource);
+        micTV = findViewById(R.id.idIVMic);
+        translateBtn = findViewById(R.id.idBtnTranslate);
+        translatedTV = findViewById(R.id.idTVTranslatedTV);
     }
 }
